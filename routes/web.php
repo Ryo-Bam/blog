a55ee834fbe226a21f199e83cec717d03c43aadf
Ryo-Bam <euratom1958.1.1.r@gmail.com>
2021-11-26 05:24:03 +0900

    削除機能の作成

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::post('/posts', 'PostController@store');
 Route::get('/posts/{post}', 'PostController@show');
 Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::put('/posts/{post}', 'PostController@update');
+Route::delete('/posts/{post}', 'PostController@delete');
 // Route::get('/', function() {
 //     return view('posts/index');
 // });
